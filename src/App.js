1d98b6aef89d2bf0c67aafc91b908414fba1af2c
NataliiaShpak <shpakn@ukr.net>
2023-06-30 13:39:58 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Nataliia Shpak and it is open-sources on {" "}
+      <a href='https://startling-moonbeam-f03261.netlify.app/' target='_blank'>
+               GitHub
+      </a>
+      </footer>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
