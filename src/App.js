f38f744c3b4a5d42db4b2cc76c7116645708ec54
NataliiaShpak <shpakn@ukr.net>
2023-07-01 21:48:41 +0200

    Added API integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>React Weather App</h1>
-      <Weather />
+      <Weather defaultCity="Kharkiv" />
       <footer>
         This project was coded by Nataliia Shpak and it is open-sources on {" "}
       <a href='https://startling-moonbeam-f03261.netlify.app/' target='_blank' rel="noreferrer">
